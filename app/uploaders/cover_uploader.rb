f3b943923fea71dafad9ea502c387c17e7985a4a
rtwhite91 <rtwhite10@gmail.com>
2021-08-05 14:55:56 -0700

    Add Author & Book models

---
+++ app/uploaders/cover_uploader.rb
@@ -0,0 +1,18 @@
+# app/uploaders/cover_uploader.rb
+class CoverUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def default_url
+    url = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/default/cover.jpg"
+    url.prepend('/') unless url[0] == '/'
+    url
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg png)
+  end
+end
